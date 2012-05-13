e4f3333e187ad73d867c3f0736cedcf3ca160c3d
David Herrmann <DavidJoel@gmx.ch>
2012-05-13 15:31:24 +0200

    cleande up

--- parser.py
+++ parser.py
@@ -6,68 +6,77 @@ class EquationError(Exception):
 
 
 class Equation():
+    """An abstract model for an equation. Solvable and
+     extendable with pyEqu"""
     def __init__(self, arguments):
         pass
 
-def cleanUp(char_list):
-    work_list = []
+class Parser():
+    def __init__(self):
+        pass
 
-    for i in range(len(char_list)):
-        if not char_list[i] == " ":
-            work_list.append(char_list[i])
-    return work_list
+    def __cleanUp(self, char_list):
+        work_list = []
 
-def toInteger(char_list):
-    work_list = []
-    nr_buf = ""
+        for i in range(len(char_list)):
+            if not char_list[i] == " ":
+                work_list.append(char_list[i])
+        return work_list
 
-    for e in char_list:
-        if ord(e) >= 48 and ord(e) <= 57:
-            nr_buf += e
-            continue
+    def __toInteger(self, char_list):
+        work_list = []
+        nr_buf = ""
+
+        for e in char_list:
+            if ord(e) >= 48 and ord(e) <= 57:
+                nr_buf += e
+                continue
+            if len(nr_buf):
+                work_list.append(nr_buf)
+                nr_buf = ""
+            work_list.append(e)
+
         if len(nr_buf):
             work_list.append(nr_buf)
-            nr_buf = ""
-        work_list.append(e)
 
-    if len(nr_buf):
-        work_list.append(nr_buf)
+        for i in range(len(work_list)):
+            try:
+                work_list[i] = int(work_list[i])
+            except ValueError:
+                continue
 
-    for i in range(len(work_list)):
-        try:
-            work_list[i] = int(work_list[i])
-        except ValueError:
-            continue
+        return work_list
 
-    return work_list
+    def __insertOperators(self, raw_list):
+        work_list = []
+        operator = 1
 
-def insertOperators(raw_list):
-    work_list = []
-    operator = 1
+        for e in raw_list:
+            if e == "-":
+                operator = -1
+            else:
+                work_list.append(operator*e)
+                operator = 1
 
-    for e in raw_list:
-        if e == "-":
-            operator = -1
-        else:
-            work_list.append(operator*e)
-            operator = 1
+        tmp_list = []
+        for i in range(len(work_list)-1):
+            if(type(work_list[i]) == int == type(work_list[i+1])):
+                tmp_list.append(work_list[i])
+                tmp_list.append("+")
+            else:
+                tmp_list.append(work_list[i])
+        tmp_list.append(work_list[-1])
+        work_list = tmp_list
 
-    tmp_list = []
-    for i in range(len(work_list)-1):
-        if(type(work_list[i]) == int == type(work_list[i+1])):
-            tmp_list.append(work_list[i])
-            tmp_list.append("+")
-        else:
-            tmp_list.append(work_list[i])
-    tmp_list.append(work_list[-1])
-    work_list = tmp_list
+        return work_list
 
-    return work_list
+    def parse(self, equation_string):
+        """Returns a solvable Equation object"""
+        equation_list = list(equation_string)
+        equation_list = self.__cleanUp(equation_list)
+        equation_list = self.__toInteger(equation_string)
+        equation_list = self.__insertOperators(equation_list)
 
-def parse(equation_string):
-    equation_list = list(equation_string)
-    equation_list = cleanUp(quation_list)
-    equation_list = toInteger(equation_string)
-    equation_list = insertOperators(equation_list)
+        return equation_list
 
 
